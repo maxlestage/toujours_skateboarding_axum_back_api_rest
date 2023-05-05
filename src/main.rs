a1de53796093820ef466263889ce903cd512b617
maxlestage <maxlestage@icloud.com>
2023-05-05 14:17:55 +0200

    Server run : ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(hello_world));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_world() -> String{
+    "Hello, World!".to_owned()
 }
